492dff255956f93be9f2a32563573a086fc30829
Ahmed <V19OAhmed8@vodafone.com>
2020-09-07 00:57:38 +0200

    update navbar and active class

--- js/app.js
+++ js/app.js
@@ -14,11 +14,9 @@
 */
 
 /**
- * Define Global Variables
  * 
-*/
-    let navbar_list = document.getElementById('navbar__list');
-    let sections = document.getElementsByTagName('section');
+ * Define Global Variables
+
 
 /**
  * End Global Variables
@@ -26,26 +24,40 @@
  * 
 */
 
+
+
 /**
  * End Helper Functions
  * Begin Main Functions
  * 
 */
 
+
+// Global Variables
+    let NavBarList = document.getElementById('navbar__list');
+    let Section = document.getElementsByTagName('section');
+
 // build the nav
 function NavBar(){
-    let CreateDom = document.createDocumentFragment();
-    let allLists = document.querySelectorAll('navbar__list');
-    let sec_Id = [];
-    for(let i = 0; i < allLists.length; i++){
-        sec_Id = section.getAttribute('id');
-        let Nav_Bar = document.createElement('li');
-       // Nav_Bar.innerHTML = `<a href= #${sec_Id}>${sec_Id}</a>`
-        Nav_Bar.innerHTML = '<a href= #%sec_Id%>%sec_Id%</a>';
-        CreateDom.appendChild(Nav_Bar);
-      
+
+    let createDom = document.createDocumentFragment();
+    let lists = document.querySelectorAll('.navbar__menu');
+    
+    for(let i = 0; i < lists.length; i++){
+       let sectionID = NavBarList.getAttribute('id');
+      // fix section name
+        console.log(sectionID + " lllll");
+        let navbar = document.createElement('li');
+        navbar.innerHTML = '<a href= #${sectionID}>${sectionID}</a>';
+        //navbar.innerHTML = `<a href= #${sectionID}>${sectionID}</a>`;
+
+        //navbar.innerHTML = `<a href= #sectionID>sectionID</a>`;
+
+        console.log(navbar + "klklklklklk");
+        createDom.appendChild(navbar);
     }
-    navbar_list.appendChild(CreateDom);
+    NavBarList.appendChild(createDom);
+
 }
 
 //NavBar();
@@ -53,18 +65,24 @@ document.addEventListener('DOMContentLoaded', NavBar);
 
 
 // Add class 'active' to section when near top of viewport
+
 function isActiveClass(){
     const lists = document.querySelectorAll('.landing__container');
     let topElement = [];
-    for(let i = 0; i < lists.length; i++){
+    let i = 0;
+    for( i = 0; i < lists.length; i++){
         let sec = lists[i];
         let rect = sec.getBoundingClientRect();
         topElement[i] = rect.top;
+        topElement.sort();
     }
     topElement.sort();
     let activeSection = lists[0];
-    activeSection.setAttribute('class','active');
-   
+    // activeSection.setAttribute('class','active');
+
+    //activeSection.setAttribute('class','active');
+    document.getElementsByTagName("section")[0].setAttribute('class','active');
+
     if(document.querySelector('section.active') != null && document.querySelector('a.active') != null){
         document.querySelector('section.active').classList.remove('active');
         document.querySelector('a.active').classList.remove('active');
@@ -72,25 +90,11 @@ function isActiveClass(){
 }
 
 // Set sections as active    
-    document.addEventListener('scroll', isActiveClass) 
-             
+     
+    document.addEventListener('scroll', isActiveClass);
+
 // Scroll to anchor ID using scrollTO event
 
-// function scrollToAnchor(e){
-//     e.preventDefault();
-//     let sec = document.querySelector('#%e.target.innerHTML%');
-//     sec.scrollToAnchor({behavior: 'smooth'});
-// }
-function scrollToAnchor(e){
-   
-   // let sec = document.querySelector(`#${e.target.innerHTML}`);
-    let sec = document.querySelector('#%e.target.innerHTML%');
-    sec.scrollToAnchor({behavior: 'smooth'});
-  
-}
-//navbar_list.addEventListener('click', scrollToAnchor(), false);
-// Scroll to section on link click
-navbar_list.addEventListener('click', scrollToAnchor,false);
 
 /**
  * End Main Functions
@@ -99,8 +103,9 @@ navbar_list.addEventListener('click', scrollToAnchor,false);
 */
 
 // Build menu 
-// window.onload = function()
-// {
-//     NavBar();
-// }
+
+// Scroll to section on link click
+
+// Set sections as active
+
 
